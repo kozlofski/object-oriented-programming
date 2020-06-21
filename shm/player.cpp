a2206174bc5166eb2df581fd08e8aa1dc89e196d
Krzysztof Kozłowski <40894665+kozlofski@users.noreply.github.com>
2020-06-21 18:34:57 +0200

    Revert "Shm10 kyrtaq"

--- shm/player.cpp
+++
@@ -1,8 +0,0 @@
-#include "player.hpp"
-
-Player::Player(const Ship& ship,
-               int money,
-               size_t availableSpace)
-    : ship_(std::make_shared<Ship>(ship)),
-      money_(money),
-      availableSpace_(availableSpace) {}
