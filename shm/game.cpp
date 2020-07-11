80da99b69d923263adaf15826bd78655ffd820e9
NG90 <glowka.natalia@gmail.com>
2020-07-11 19:52:42 +0200

    SHM21: adapt Game class to changes in Time and Player

--- shm/game.cpp
+++ shm/game.cpp
@@ -1,28 +1,29 @@
 #include "game.hpp"
 
+#include "shm_time.hpp"
+
 #include <iostream>
+#include <memory>
 #include <string>
 
 const std::string line(80, '*');
-constexpr int shipMaxCrew = 50;
-constexpr int shipSpeed = 10;
-constexpr size_t shipId = 7;
-const std::string& shipName = "Black Perl";
-constexpr int shipCapacity = 250;
 
 Game::Game(size_t money, size_t days, size_t finalGoal)
     : money_(money), days_(days), finalGoal_(finalGoal), currentDay_(0) {}
 
-void Game::startGame() {
-    auto gameShip = std::make_shared<Ship>(shipMaxCrew, shipSpeed, shipId, shipName, shipCapacity);
-    player_ = std::make_shared<Player>(gameShip, money_, 0);
+void Game::startGame()
+{
+    auto time = std::make_shared<Time>();
+    player_ = std::make_shared<Player>(money_, time.get());
     map_ = std::make_shared<Map>();
+
     std::cout << "Welcome in SHM game, you have " << days_ << " days to earn "
               << finalGoal_ << ".\nGOOD LUCK!\n\n";
     printMenu();
 }
 
-void Game::printMenu() {
+void Game::printMenu()
+{
     std::cout << line << "\n\n";
     //TODO: print data from Time class!!!
     std::cout << "Money: " << player_->getMoney()
