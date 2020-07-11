842d84b6f0d0024b3721c39e350c31c245fa56dc
NG90 <glowka.natalia@gmail.com>
2020-07-11 23:50:10 +0200

    SHM21: Map class extensions

--- shm/map.hpp
+++ shm/map.hpp
@@ -8,15 +8,21 @@ constexpr size_t totalIslands = 10;
 constexpr size_t mapWidth = 100;
 constexpr size_t mapHeight = 100;
 
+class Time;
+
 class Map {
 public:
-    Map();
+    Map(Time* timeObserver);
     Island* getIsland(const Island::Coordinates& coordinates);
     Island* getCurrentPosition() const { return currentPosition_; };
+
+    size_t getDistanceToIsland(const Island* destination) const;
+    void travel(Island* destination);
+    friend std::ostream& operator<<(std::ostream& output, const Map& map);
 
 private:
     Island* currentPosition_{};
     std::vector<Island> islands_{};
 
-    void generateIsland(size_t howMany);
+    void generateIsland(size_t howMany, Time* timeObserver);
 };
